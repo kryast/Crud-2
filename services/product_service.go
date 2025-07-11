f720357cb98da393a938b052936de70c700d2f97
Ahmad Syarifuddin <oden.psht@gmail.com>
2025-07-11 10:14:30 +0700

    Create Product Services

---
+++ services/product_service.go
@@ -0,0 +1,14 @@
+package services
+
+import "github.com/kryast/Crud-2.git/repositories"
+
+type ProductService interface {
+}
+
+type productService struct {
+	repo repositories.ProductRepository
+}
+
+func NewProductService(r repositories.ProductRepository) ProductService {
+	return &productService{r}
+}
